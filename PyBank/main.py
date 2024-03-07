6f9b284eb8e0922a0ffb2d65a4e28e1bd79f0f6f
eugeniocez <eugenio.cez@gmail.com>
2024-03-06 20:36:13 -0600

    added comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,11 @@
 import os
 import csv
 
+#path to csv file
+
 csvpath=os.path.join('Resources','budget_data.csv')
+
+#initializing variables
 
 total_months=0
 net_total=0
@@ -10,18 +14,34 @@ total_change=0
 greatest_increase=["",0]
 greatest_decrease=["",9999999999]
 
+#read csv file
+
 with open(csvpath,newline="") as csvfile:
     csvreader=csv.reader(csvfile)
 
+    #skip header row
+
     csv_header=next(csvreader)
 
+    #loop through rows in csv file
+
     for row in csvreader:
+
+        #update total months
+
         total_months=total_months+1
+
+        #update net total
+
         net_total=net_total+int(row[1])
+
+        #calculate change in profits and losses
 
         if total_months>1:
             change=int(row[1])-prev_profit_loss
             total_change=total_change+change
+
+            #check greatest incrase and decrease
 
             if change>greatest_increase[1]:
                 greatest_increase=[row[0],change]
@@ -29,10 +49,14 @@ with open(csvpath,newline="") as csvfile:
             if change<greatest_decrease[1]:
                 greatest_decrease=[row[0],change]
 
+        #update previous profit or loss
         prev_profit_loss=int(row[1])
 
+#calculate average change
+        
 average_change=total_change/(total_months-1)
 
+#print analysis in terminal and export results to a text file
 output_file=os.path.join('Analysis','financial_analysis.txt')
 
 print('Financial Analysis')
